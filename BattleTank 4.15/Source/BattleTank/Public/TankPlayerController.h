b04edddfb7eca2686a6dc29eaff1b2610dd696b3
josh thorn <joshthorn91@gmail.com>
2017-04-27 13:35:52 +0100

    148
    Changing EditAnywhere to EditDefaultsOnly

--- BattleTank 4.15/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank 4.15/Source/BattleTank/Public/TankPlayerController.h
@@ -29,12 +29,12 @@ private:
 	bool GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const;
 	bool GetLookVectorHitLocation(FVector LookDirection, FVector& HitLocation) const;
 
-	UPROPERTY(EditAnywhere)
+	UPROPERTY(EditDefaultsOnly)
 	float CrossHairXLocation = 0.5f;
 
-	UPROPERTY(EditAnywhere)
+	UPROPERTY(EditDefaultsOnly)
 	float CrossHairYLocation = 0.33333f;
 
-	UPROPERTY(EditAnywhere)
+	UPROPERTY(EditDefaultsOnly)
 	float LineTraceRange = 1000000;
 };
