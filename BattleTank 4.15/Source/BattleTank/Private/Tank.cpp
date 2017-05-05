87555f3b693c3cca9cac36369f869f642f8e6ff5
josh thorn <joshthorn91@gmail.com>
2017-05-05 16:35:41 +0100

    155
    Gamepad issue due to directinput controller. UE needs xinput.

--- BattleTank 4.15/Source/BattleTank/Private/Tank.cpp
+++ BattleTank 4.15/Source/BattleTank/Private/Tank.cpp
@@ -18,8 +18,6 @@ ATank::ATank()
 
 	//no need to protect pointers, added at construction
 	TankAimingComponent = CreateDefaultSubobject<UTankAimingComponent>(FName("Aiming component"));
-	TankMovementComponent = CreateDefaultSubobject<UTankMovementComponent>(FName("Movement component"));
-
 }
 
 void ATank::SetBarrelReference(UTankBarrel * BarrelToSet)
