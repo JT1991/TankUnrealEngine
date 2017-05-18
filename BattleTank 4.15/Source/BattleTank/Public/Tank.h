a10868f6a704fd998466dc6f96a39b53bd0f79d8
josh thorn <joshthorn91@gmail.com>
2017-05-18 14:33:03 +0100

    170

--- BattleTank 4.15/Source/BattleTank/Public/Tank.h
+++ BattleTank 4.15/Source/BattleTank/Public/Tank.h
@@ -10,7 +10,6 @@ class UTankBarrel;
 class UTankTurret;
 class UTankAimingComponent;
 class AProjectile;
-class UTankMovementComponent;
 
 UCLASS()
 class BATTLETANK_API ATank : public APawn
@@ -26,8 +25,6 @@ public:
 protected:
 	UPROPERTY(BlueprintReadOnly)
 	UTankAimingComponent* TankAimingComponent = nullptr;
-	UPROPERTY(BlueprintReadOnly)
-	UTankMovementComponent* TankMovementComponent = nullptr;
 
 private:
 	// Sets default values for this pawn's properties
