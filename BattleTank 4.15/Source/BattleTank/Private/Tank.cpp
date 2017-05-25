c33ae1ba5f0c0f438f786a17a432dd445453ae20
josh thorn <joshthorn91@gmail.com>
2017-05-25 15:27:36 +0100

    174

--- BattleTank 4.15/Source/BattleTank/Private/Tank.cpp
+++ BattleTank 4.15/Source/BattleTank/Private/Tank.cpp
@@ -8,6 +8,8 @@ ATank::ATank()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
+	UE_LOG(LogTemp, Warning, TEXT("NOT HERE"));
+
 }
 
 
