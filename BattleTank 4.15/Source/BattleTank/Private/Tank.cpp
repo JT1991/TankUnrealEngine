48f1f4d16456a4ffe65f8d12bfa7bc54e36c04f8
josh thorn <joshthorn91@gmail.com>
2017-05-25 14:53:07 +0100

    174

--- BattleTank 4.15/Source/BattleTank/Private/Tank.cpp
+++ BattleTank 4.15/Source/BattleTank/Private/Tank.cpp
@@ -1,8 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "BattleTank.h"
-#include "TankBarrel.h"
-#include "Projectile.h"
 #include "Tank.h"
 
 // Sets default values
@@ -12,24 +10,4 @@ ATank::ATank()
 	PrimaryActorTick.bCanEverTick = false;
 }
 
-void ATank::BeginPlay()
-{
-	Super::BeginPlay(); //NEEDED FOR BP BEGINPLAY TO RUN
-}
 
-void ATank::Fire() 
-{
-	if (!ensure(Barrel)) { return; }
-	bool isReloaded = (FPlatformTime::Seconds() - LastFireTime) > ReloadTimeSeconds;
-	if (isReloaded) 
-	{
-		//spawn projectile at socket location
-		auto Projectile = GetWorld()->SpawnActor<AProjectile>(
-			ProjectileBlueprint,
-			Barrel->GetSocketLocation(FName("Projectile")),
-			Barrel->GetSocketRotation(FName("Projectile"))
-			);
-		Projectile->LaunchProjectile(LaunchSpeed);
-		LastFireTime = FPlatformTime::Seconds();
-	}
-}
