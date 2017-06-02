1772bd20976272a0888d34935433c7238528563f
josh thorn <joshthorn91@gmail.com>
2017-06-02 15:13:03 +0100

    191

--- BattleTank 4.15/Source/BattleTank/Public/Tank.h
+++ BattleTank 4.15/Source/BattleTank/Public/Tank.h
@@ -11,10 +11,14 @@ class BATTLETANK_API ATank : public APawn
 	GENERATED_BODY()
 
 public:
-
+	//called by engine when actor damage is dealt
+	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const & DamageEvent, class AController * EventInstigator, AActor * DamageCauser) override;
 
 private:
 	// Sets default values for this pawn's properties
 	ATank();
-
+	UPROPERTY(EditDefaultsOnly, Category = Setup)
+	int32 StartingHealth = 100;
+	UPROPERTY(VisibleAnywhere, Category = Health)
+	int32 CurrentHealth = StartingHealth;
 };
